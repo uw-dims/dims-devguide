26084486f1f579554eb6d27368fbb827826f37b6
Dave Dittrich <dittrich@u.washington.edu>
2015-07-19 18:19:16 -0700

    Bump version: 0.0.13 → 0.0.14

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,7 +66,7 @@ author = u'Dave Dittrich'
 # built documents.
 #
 # The short X.Y version.
-version = '0.0.13'
+version = '0.0.14'
 
 # The full version, including alpha/beta/rc tags.
 release = version
